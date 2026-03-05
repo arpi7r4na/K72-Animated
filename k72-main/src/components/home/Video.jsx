def02d36f2ea192240329ff87c3b5e9b23f45544
Arpit Rana <arpit132rana@gmail.com>
2026-03-05 07:27:52 +0530

    changed in Video

--- k72-main/src/components/home/Video.jsx
+++ k72-main/src/components/home/Video.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import videoFile from "../../assets/video.mp4";
 const Video = () => {
   return (
     <div className='h-full w-full'>
-        <video className='h-full w-full object-cover' autoPlay loop muted src="../../../public/video.mp4"></video>
+        <video
+          className="h-full w-full object-cover"
+          autoPlay
+          loop
+          muted
+          playsInline
+          preload="auto"
+          src={videoFile}
+        />
     </div>
   )
 }
